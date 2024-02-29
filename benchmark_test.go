f21107f2855e79528011bd0f489a0e2e90b2249d
gpsanant <gpsanant@gmail.com>
2024-02-28 16:18:43 -0800

    make tests really check

--- benchmark_test.go
+++ benchmark_test.go
@@ -3,22 +3,45 @@ package eigenpodproofs
 import (
 	"testing"
 
+	"github.com/Layr-Labs/eigenpod-proofs-generation/beacon"
 	"github.com/attestantio/go-eth2-client/spec"
 	"github.com/attestantio/go-eth2-client/spec/phase0"
 	"github.com/stretchr/testify/assert"
 )
 
 func BenchmarkComputeBeaconStateRoot(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		_, _ = epp.ComputeBeaconStateRoot(&oracleState)
+	computed, err := epp.ComputeBeaconStateRoot(&oracleState)
+	if err != nil {
+		b.Fatal(err)
 	}
 
+	var cached phase0.Root
+	for i := 0; i < b.N; i++ {
+		cached, err = epp.ComputeBeaconStateRoot(&oracleState)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+
+	assert.Equal(b, computed, cached)
 }
 
 func BenchmarkComputeBeaconStateTopLevelRoots(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		_, _ = epp.ComputeBeaconStateTopLevelRoots(&spec.VersionedBeaconState{Deneb: &oracleState})
+	computed, err := epp.ComputeBeaconStateTopLevelRoots(&spec.VersionedBeaconState{Deneb: &oracleState})
+	if err != nil {
+		b.Fatal(err)
 	}
+
+	var cached *beacon.BeaconStateTopLevelRoots
+	for i := 0; i < b.N; i++ {
+		cached, err = epp.ComputeBeaconStateTopLevelRoots(&spec.VersionedBeaconState{Deneb: &oracleState})
+		if err != nil {
+			b.Fatal(err)
+		}
+
+	}
+
+	assert.Equal(b, computed, cached)
 }
 
 func BenchmarkComputeValidatorTree(b *testing.B) {
@@ -36,5 +59,4 @@ func BenchmarkComputeValidatorTree(b *testing.B) {
 	}
 
 	assert.Equal(b, computed, cached)
-
 }
