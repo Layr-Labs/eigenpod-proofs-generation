3460f3887259b9a16389e09c33a6a3a36bf85760
Justin Brower <jbrower95@gmail.com>
2024-07-30 12:44:11 -0400

    add wasm support

--- cli/main_js.go
+++ cli/main_js.go
@@ -4,67 +4,111 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"math/big"
 	"syscall/js"
 
 	eigenpodproofs "github.com/Layr-Labs/eigenpod-proofs-generation"
-	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/Layr-Labs/eigenpod-proofs-generation/cli/core"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
-func getClients(node, beaconNodeUri string) (*ethclient.Client, BeaconClient, *big.Int) {
+func getClients(node, beaconNodeUri string) (*ethclient.Client, core.BeaconClient, *big.Int, error) {
 	eth, err := ethclient.Dial(node)
-	PanicOnError("failed to reach eth --node.", err)
+	if err != nil {
+		return nil, nil, nil, fmt.Errorf("failed to reach eth --node: %w", err)
+	}
 
 	chainId, err := eth.ChainID(context.Background())
-	PanicOnError("failed to fetch chain id", err)
+	if err != nil {
+		return nil, nil, nil, fmt.Errorf("failed to fetch chain id: %w", err)
+	}
 
-	beaconClient, err := getBeaconClient(beaconNodeUri)
-	PanicOnError("failed to reach beacon chain.", err)
+	beaconClient, err := core.GetBeaconClient(beaconNodeUri)
+	if err != nil {
+		return nil, nil, nil, fmt.Errorf("failed to reach beacon chain: %w", err)
+	}
 
-	return eth, beaconClient, chainId
+	return eth, beaconClient, chainId, nil
 }
 
 func Checkpoint(this js.Value, args []js.Value) any {
 	eigenpodAddress := args[0].String()
 	beacon := args[1].String()
 	node := args[2].String()
-	owner := args[3].String()
 
-	var eth, beaconClient, chainId = getClients(node, beacon)
-	var ownerValue *string = nil
-	if len(owner) > 0 {
-		ownerValue = &owner
-	}
+	handler := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		resolve := args[0]
+		reject := args[1]
 
-	proofs := RunCheckpointProof(context.Background(), eigenpodAddress, eth, chainId, beaconClient, ownerValue)
-	return *proofs
+		go func() {
+			var eth, beaconClient, chainId, err = getClients(node, beacon)
+			if err != nil {
+				errorConstructor := js.Global().Get("Error")
+				errorObject := errorConstructor.New(err.Error())
+				reject.Invoke(errorObject)
+				return
+			}
+
+			proofs, err := core.GenerateCheckpointProof(context.Background(), eigenpodAddress, eth, chainId, beaconClient)
+
+			if err != nil {
+				errorConstructor := js.Global().Get("Error")
+				errorObject := errorConstructor.New(err.Error())
+				reject.Invoke(errorObject)
+			} else {
+				resolve.Invoke(js.ValueOf(proofs))
+			}
+		}()
+
+		return nil
+	})
+
+	promiseConstructor := js.Global().Get("Promise")
+	return promiseConstructor.New(handler)
 }
 
 type TValidatorProofStruct = struct {
-	Proof            eigenpodproofs.VerifyValidatorFieldsCallParams
-	ValidatorIndices []uint64
-	LatestBlock      types.Block
+	Proof           eigenpodproofs.VerifyValidatorFieldsCallParams
+	BeaconTimestamp uint64
 }
 
 func Validator(this js.Value, args []js.Value) any {
 	eigenpodAddress := args[0].String()
 	beacon := args[1].String()
 	node := args[2].String()
-	owner := args[3].String()
 
-	var eth, beaconClient, chainId = getClients(node, beacon)
-	var ownerValue *string = nil
-	if len(owner) > 0 {
-		ownerValue = &owner
-	}
+	handler := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		resolve := args[0]
+		reject := args[1]
 
-	proofs, validatorIndices, block := RunValidatorProof(context.Background(), eigenpodAddress, eth, chainId, beaconClient, ownerValue)
-	return TValidatorProofStruct{
-		Proof:            *proofs,
-		ValidatorIndices: validatorIndices,
-		LatestBlock:      *block,
-	}
+		go func() {
+			var eth, beaconClient, chainId, err = getClients(node, beacon)
+			if err != nil {
+				errorConstructor := js.Global().Get("Error")
+				errorObject := errorConstructor.New(err.Error())
+				reject.Invoke(errorObject)
+				return
+			}
+
+			proofs, beaconTimestamp, err := core.GenerateValidatorProof(context.Background(), eigenpodAddress, eth, chainId, beaconClient)
+			if err != nil {
+				errorConstructor := js.Global().Get("Error")
+				errorObject := errorConstructor.New(err.Error())
+				reject.Invoke(errorObject)
+			} else {
+				resolve.Invoke(js.ValueOf(TValidatorProofStruct{
+					Proof:           *proofs,
+					BeaconTimestamp: beaconTimestamp,
+				}))
+			}
+		}()
+
+		return nil
+	})
+
+	promiseConstructor := js.Global().Get("Promise")
+	return promiseConstructor.New(handler)
 }
 
 func main() {
